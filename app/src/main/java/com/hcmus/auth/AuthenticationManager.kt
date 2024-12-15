df8a44c9e8f9e33304fdb27cebd9a09dba330c6e
cukhoaimon <phucmapcaumieu@gmail.com>
2024-12-15 17:13:03 +0700

    cmt

--- app/src/main/java/com/hcmus/auth/AuthenticationManager.kt
+++ app/src/main/java/com/hcmus/auth/AuthenticationManager.kt
@@ -1,36 +1,36 @@
-package com.hcmus.auth
-
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-
-interface AuthResponse {
-  data object Success: AuthResponse
-  data class Error(val message: String): AuthResponse
-}
-
-class AuthenticationManager {
-  private val auth = Firebase.auth
-
-  fun createAccountWithEmail(email: String, password: String): Flow<AuthResponse> = callbackFlow {
-    auth.createUserWithEmailAndPassword(email, password)
-      .addOnCompleteListener { task ->
-        if (task.isSuccessful) {
-          trySend(AuthResponse.Success)
-        } else {
-          trySend(AuthResponse.Error(task.exception?.message ?: "Unknown error"))
-        }
-      }
-  }
-
-  fun loginWithEmail(email: String, password: String): Flow<AuthResponse> = callbackFlow {
-    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-      if (task.isSuccessful) {
-        trySend(AuthResponse.Success)
-      } else {
-        trySend(AuthResponse.Error(task.exception?.message ?: "Unknown error"))
-      }
-    }
-  }
-}
+//package com.hcmus.auth
+//
+//import com.google.firebase.Firebase
+//import com.google.firebase.auth.auth
+//import kotlinx.coroutines.flow.Flow
+//import kotlinx.coroutines.flow.callbackFlow
+//
+//interface AuthResponse {
+//  data object Success: AuthResponse
+//  data class Error(val message: String): AuthResponse
+//}
+//
+//class AuthenticationManager {
+//  private val auth = Firebase.auth
+//
+//  fun createAccountWithEmail(email: String, password: String): Flow<AuthResponse> = callbackFlow {
+//    auth.createUserWithEmailAndPassword(email, password)
+//      .addOnCompleteListener { task ->
+//        if (task.isSuccessful) {
+//          trySend(AuthResponse.Success)
+//        } else {
+//          trySend(AuthResponse.Error(task.exception?.message ?: "Unknown error"))
+//        }
+//      }
+//  }
+//
+//  fun loginWithEmail(email: String, password: String): Flow<AuthResponse> = callbackFlow {
+//    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+//      if (task.isSuccessful) {
+//        trySend(AuthResponse.Success)
+//      } else {
+//        trySend(AuthResponse.Error(task.exception?.message ?: "Unknown error"))
+//      }
+//    }
+//  }
+//}
